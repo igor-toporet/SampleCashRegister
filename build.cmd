49074db7a5889fcca7762a7073eeeddac5349033
Igor Toporet <igor@toporet>
2012-06-14 23:00:01 -0700

    deleted old cmd files

--- build.cmd
+++
@@ -1,1 +0,0 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /t:Rebuild /p:Configuration=Release /v:m SampleCashRegister.sln
--- run-unit-tests.cmd
+++
@@ -1,3 +0,0 @@
-Lib\xUnit.Net-1.7\xunit.console.clr4.exe Src\UnitTests\bin\Release\SampleCashRegister.UnitTests.dll /html test-run.html
-
-test-run.html
